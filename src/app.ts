0712e656d8cf3bd626c6f62dcee3c36ed9fe1617
suryapratap-1 <suryapratap0765@gmail.com>
2025-05-07 12:52:35 +0530

    Add limit to the size of the request body

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 export const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan("dev"));
